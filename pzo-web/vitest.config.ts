9aba5302e49220f1174bc97702d1b685c48be013
Antonio T. Smith Jr. <antonio0720@users.noreply.github.com>
2026-03-08 21:05:48 -0500

    Newest version, most accurate version of the game — 2026-03-08 21:05:48

--- pzo-web/vitest.config.ts
+++ pzo-web/vitest.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()] as any,
   test: {
     globals: true,
     environment: 'node',
-    include: ['src/**/*.test.ts'],
+    include: [
+      'src/**/*.{test,spec}.{ts,tsx}',
+      'test/**/*.{test,spec}.{ts,tsx}',
+      'test/unit/**/*.{test,spec}.{ts,tsx}',
+    ],
     coverage: {
       reporter: ['text', 'json'],
     },
   },
-})
+});
